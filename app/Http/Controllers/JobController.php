6bffb4bccc9b86167e0e74403b6aad03f6d8600e
omaomach <aaronjoash44@gmail.com>
2025-01-22 15:29:42 +0300

    worked on generating a controller with the primary methods and creating a resource route

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -6,34 +6,71 @@ use Illuminate\Http\Request;
 
 class JobController extends Controller
 {
-    public function index() // This will list all the jobs
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        $title = 'Available Jobs';
         $jobs = [
             'Web Developer',
-            'Database Admin',
+            'Database Administrator',
             'Software Engineer',
-            'Systems Analyst'
+            'Business Analyst',
         ];
 
-        return view('jobs.index', compact('title', 'jobs'));
+        return view('jobs.index', compact('jobs'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
         return view('jobs.create');
     }
 
-    public function show(string $id)
-    {
-        return "Showing job $id";
-    }
-
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(Request $request)
     {
-        $title = $request->input('title');
-        $description = $request->input('description');
+        //
+    }
 
-        return "Title: $title, Description: $description";
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function share()
+    {
+        return 'Share this job';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,4 @@ use App\Http\Controllers\JobController;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{id}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
+Route::resource('jobs', JobController::class);
