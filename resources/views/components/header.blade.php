0b4b78262c57976c55b757d1e2b068700eb20fc4
omaomach <aaronjoash44@gmail.com>
2025-01-28 19:15:29 +0300

    worked on components and styling

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,17 +4,14 @@
             <a href="{{ url('/') }}">Workopia</a>
         </h1>
         <nav class="hidden md:flex items-center space-x-4">
-            <a href="{{ url('/jobs') }}" class="text-white hover:underline py-2">All Jobs</a>
-            <a href="{{ url('/jobs/saved') }}" class="text-white hover:underline py-2">Saved Jobs</a>
-            <a href="{{ url('/login') }}" class="text-white hover:underline py-2">Login</a>
-            <a href="{{ url('/register') }}" class="text-white hover:underline py-2">Register</a>
-            <a href="{{ url('/dashboard') }}" class="text-white hover:underline py-2">
-                <i class="fa fa-gauge mr-1"></i> Dashboard
-            </a>
-            <a href="{{ url('/jobs/create') }}"
-                class="bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300">
-                <i class="fa fa-edit"></i> Create Job
-            </a>
+            <x-nav-link url="/" :active="request()->is('/')">Home</x-nav-link>
+            <x-nav-link url="/jobs" :active="request()->is('jobs')">All Jobs</x-nav-link>
+            <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')">Saved Jobs</x-nav-link>
+            <x-nav-link url="/login" :active="request()->is('login')">Login</x-nav-link>
+            <x-nav-link url="/register" :active="request()->is('register')">Register</x-nav-link>
+            <x-nav-link url="/dashboard" :active="request()->is('dashboard')" icon="gauge">Dashboard</x-nav-link>
+            <x-button-link url="/jobs/create" bgClass="bg-red-500" textClass="text-white" icon="edit">Create
+                Job</x-button-link>
         </nav>
         <button id="hamburger" class="text-white md:hidden flex items-center">
             <i class="fa fa-bars text-2xl"></i>
@@ -22,15 +19,11 @@
     </div>
     <!-- Mobile Menu -->
     <nav id="mobile-menu" class="hidden md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
-        <a href="{{ url('/jobs') }}" class="block px-4 py-2 hover:bg-blue-700">All Jobs</a>
-        <a href="{{ url('/jobs/saved') }}" class="block px-4 py-2 hover:bg-blue-700">Saved Jobs</a>
-        <a href="{{ url('/login') }}" class="block px-4 py-2 hover:bg-blue-700">Login</a>
-        <a href="{{ url('/register') }}" class="block px-4 py-2 hover:bg-blue-700">Register</a>
-        <a href="{{ url('/dashboard') }}" class="block text-white hover:underline py-2">
-            <i class="fa fa-gauge mr-1"></i> Dashboard
-        </a>
-        <a href="{{ url('/jobs/create') }}" class="block px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-black">
-            <i class="fa fa-edit"></i> Create Job
-        </a>
+        <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile='true'>All Jobs</x-nav-link>
+        <x-nav-link url="/jobs/saved" :active="request()->is('jobs/saved')" :mobile='true'>Saved Jobs</x-nav-link>
+        <x-nav-link url="/login" :active="request()->is('login')" :mobile='true'>Login</x-nav-link>
+        <x-nav-link url="/register" :active="request()->is('register')" :mobile='true'>Register</x-nav-link>
+        <x-nav-link url="/dashboard" :active="request()->is('dashboard')" :mobile='true'>Dashboard</x-nav-link>
+        <x-button-link url="/jobs/create" icon="edit" :block='true'>Create Job</x-button-link>
     </nav>
 </header>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,14 +8,21 @@
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
     <title>{{ $title ?? 'Workopia | Find and list jobs' }}</title>
 </head>
 
 <body class="bg-gray-100">
     <x-header />
+    @if (request()->is('/'))
+        <x-hero title="Find Your Dream Job" />
+        <x-top-banner />
+    @endif
     <main class="container mx-auto p-4 mt-4">
         {{ $slot }}
     </main>
+
+    <script src="{{ asset('js/script.js') }}"></script>
 </body>
 
 </html>
