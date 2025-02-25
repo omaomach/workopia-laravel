1ee65d96e2dbdfa9eeedb57236a435e95e565cd2
omaomach <aaronjoash44@gmail.com>
2025-02-25 16:22:01 +0300

    create login and register controllers and routes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,8 +8,13 @@ use Illuminate\View\View;
 
 class HomeController extends Controller
 {
+    // Show home index view
+    // @route GET /
     public function index(): View
     {
+        // session()->put('test', '123');
+        // session()->forget('test');
+
         $jobs = Job::latest()->limit(6)->get();
 
         return view('pages.index')->with('jobs', $jobs);
