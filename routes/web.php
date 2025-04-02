f5be7fbe1c4d4ec086cf14c9d437a9805663d460
omaomach <aaronjoash44@gmail.com>
2025-04-02 18:36:14 +0300

    feat: worked on the profile controller and info update

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
 // use App\Http\Middleware\LogRequest;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -32,4 +33,8 @@ Route::get('/test', function () {
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->name('dashboard')
+    ->middleware('auth');
+
+Route::put('/profile', [ProfileController::class, 'update'])
+    ->name('profile.update')
     ->middleware('auth');
