ca7e2712163f58635a76abbea0dd7dba6fe15a24
omaomach <aaronjoash44@gmail.com>
2025-04-09 21:03:30 +0300

    feat: worked on showing applicants and downloading resumes

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -56,6 +56,37 @@
                         </form>
                     </div>
                 </div>
+
+                {{-- Applicants --}}
+                <div class="mt-4">
+                    <h4 class="text-lg font-semibold mb-2">Applicants</h4>
+                    @forelse($job->applicants as $applicant)
+                        <div class="py-2">
+                            <p class="text-gray-800">
+                                <strong>Name: </strong> {{ $applicant->full_name }}
+                            </p>
+                            <p class="text-gray-800">
+                                <strong>Phone: </strong> {{ $applicant->contact_phone }}
+                            </p>
+                            <p class="text-gray-800">
+                                <strong>Email: </strong> {{ $applicant->contact_email }}
+                            </p>
+                            <p class="text-gray-800">
+                                <strong>Message: </strong> {{ $applicant->message }}
+                            </p>
+                            <p class="text-gray-800 my-4">
+                                <a href="{{ asset('storage/' . $applicant->resume_path) }}"
+                                    class="text-blue-500 hover:underline" download>
+                                    <i class="fas fa-download"></i> Download Resume
+                                </a>
+                            </p>
+                        </div>
+                    @empty
+                        <p class="text-gray-700">No Applicants for this job</p>
+                    @endforelse
+
+                </div>
+
             @empty
                 <p class="text-gray-700">You have no job listings</p>
             @endforelse
