3978c5a639ed2a4f82165ac257ea34058435a970
omaomach <aaronjoash44@gmail.com>
2025-04-11 14:28:51 +0300

    feat: configured search functionality

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -3,6 +3,14 @@
         <x-search />
     </div>
 
+    {{-- Back Button --}}
+    @if (request()->has('keywords') || request()->has('location'))
+        <a href="{{ route('jobs.index') }}"
+            class="bg-gray-700 hover:bg-gray-600 text-white px-4 py-2 rounded mb-4 inline-block">
+            <i class="fa fa-arrow-left mr-1"></i> Back
+        </a>
+    @endif
+
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         @forelse ($jobs as $job)
             <x-job-card :job="$job" />
