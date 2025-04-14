50a65eb0ff958437c45aa85ce2719953712a6b29
omaomach <aaronjoash44@gmail.com>
2025-04-14 15:56:23 +0300

    dev: integrated MapBox

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\BookmarkController;
 use App\Http\Controllers\ApplicantController;
+use App\Http\Controllers\GeocodeController;
 // use App\Http\Middleware\LogRequest;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -55,3 +56,5 @@ Route::post('/jobs/{job}/apply', [ApplicantController::class, 'store'])
 Route::delete('/applicants/{applicant}', [ApplicantController::class, 'destroy'])
     ->name('applicant.destroy')
     ->middleware('auth');
+
+Route::get('/geocode', [GeocodeController::class, 'geocode']);
